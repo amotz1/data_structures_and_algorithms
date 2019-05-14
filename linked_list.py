89d8f008e2929524277b7ff585606a020d84540e
amotz taub tabib <amotz.tt@gmail.com>
2019-05-14 16:14:47 +0300

    version with iterators and tail changes

--- linked_list.py
+++ linked_list.py
@@ -1,9 +1,9 @@
 # implementing Link and LinkedList
 
 
-class Link:
-    def __init__(self, link_value):
-        self.link_value = link_value
+class _Link:
+    def __init__(self, value):
+        self.value = value
         self.next = None
 
     def set_next(self, new_next):
@@ -12,81 +12,117 @@ class Link:
     def get_next(self):
         return self.next
 
-    def get_data(self):
-        return self.link_value
+    def get_value(self):
+        return self.value
 
 
 class LinkedList:
     def __init__(self):
-        self.head_link = None
+        self._head_link = None
+        self._last_link = None
 
     def is_empty(self):
-        return self.head_link is None
+        return self._head_link is None
 
-    def __find_link(self, link_value):
-        link = self.head_link
+    def _find_link(self, value):
+        link = self._head_link
         while link is not None:
-            if link.get_data() == link_value:
+            if link.get_value() == value:
                 return link
             else:
                 link = link.get_next()
+        return None
 
-    def add_link_at_start(self, link_value):
-        new_link = Link(link_value)
-        new_link.set_next(self.head_link)
-        self.head_link = new_link
-
-    def add_link_at_end(self, link_value):
-        new_link = Link(link_value)
-        if self.head_link is None:
-            self.head_link = new_link
-        else:
-            link = self.head_link
-            while link.get_next() is not None:
+    def test_find_link(self, value):
+        link = self._head_link
+        while link is not None:
+            if link.get_value() == value:
+                return True
+            else:
                 link = link.get_next()
-            link.set_next(new_link)
+        return False
 
-    def remove(self, link_value):
-        link_to_find = self.__find_link(link_value)
-        if link_to_find == self.head_link:
-            self.head_link = link_to_find.get_next()
-            link_to_find.set_next(None)
+    def add_link_at_start(self, value):
+        new_link = _Link(value)
+        new_link.set_next(self._head_link)
+        self._head_link = new_link
+
+    def add_link_at_end(self, value):
+        new_link = _Link(value)
+        if self._head_link is None:
+            self._head_link = new_link
+            self._last_link = self._head_link
         else:
-            previous_link = self.head_link
+            self._last_link.set_next(new_link)
+            self._last_link = new_link
+            new_link.set_next(None)
+
+    def remove(self, value):
+        link_to_find = self._find_link(value)
+        if link_to_find == self._head_link:
+            self._head_link = link_to_find.get_next()
+            link_to_find.set_next(None)
+        elif link_to_find == self._last_link:
+            previous_link = self._head_link
+            while previous_link.get_next() is not link_to_find:
+                previous_link = previous_link.get_next()
+            previous_link.set_next(None)
+        else:
+            previous_link = self._head_link
             while previous_link.get_next() is not link_to_find:
                 previous_link = previous_link.get_next()
             previous_link.set_next(link_to_find.get_next())
             link_to_find.set_next(None)
 
-    def traverse_Linked_List(self):
-        link = self.head_link
-        print("the values in the linked_list are:")
-        while link is not None:
-            print(link.link_value)
-            link = link.get_next()
+    def print_collection(self):
+        new_itterator = _Counter(self._head_link)
+        for i in new_itterator:
+            print(i)
 
-    def add_link_after_link(self, link_to_add, link_to_find):
-        link_to_find = self.__find_link(link_to_find)
-        new_link = Link(link_to_add)
-        new_link.set_next(link_to_find.get_next())
-        link_to_find.set_next(new_link)
+    def add_link_after(self, link_to_add, link_to_find):
+        link_to_find = self._find_link(link_to_find)
+        if link_to_find == self._last_link:
+            new_link = _Link(link_to_add)
+            self._last_link.set_next(new_link)
+            self._last_link = new_link
+        else:
+            new_link = _Link(link_to_add)
+            new_link.set_next(link_to_find.get_next())
+            link_to_find.set_next(new_link)
+
+
+class _Counter:
+    def __init__(self, current):
+        self._current = current
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._current is None:
+            raise StopIteration
+        else:
+            value = self._current
+            self._current = self._current.get_next()
+            return value.value
 
 
 def test_Linked_List():
     my_list = LinkedList()
     my_list.add_link_at_end("Yotam")
     assert (not my_list.is_empty())
-    my_list.traverse_Linked_List()
     my_list.add_link_at_end("Amotz")
-    my_list.traverse_Linked_List()
     my_list.add_link_at_end("Anat")
-    my_list.traverse_Linked_List()
     my_list.add_link_at_start("hillel")
-    my_list.traverse_Linked_List()
+    assert my_list.test_find_link("Amotz")
     my_list.remove("Amotz")
-    my_list.traverse_Linked_List()
-    my_list.add_link_after_link("Asaf", "Anat")
-    my_list.traverse_Linked_List()
+    assert not my_list.test_find_link("Amotz")
+    my_list.add_link_after("Asaf", "Anat")
+    new_itterator = _Counter(my_list._head_link)
+    assert new_itterator.__next__() == "hillel"
+    assert new_itterator.__next__() == "Yotam"
+    assert new_itterator.__next__() == "Anat"
+    assert new_itterator.__next__() == "Asaf"
 
 
 test_Linked_List()
