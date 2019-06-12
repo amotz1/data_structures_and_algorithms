ffce7cf8c9a2a0b44b922846efb4288846d34ab0
amotz1 <31002831+amotz1@users.noreply.github.com>
2019-06-12 15:38:40 +0300

    Add files via upload

--- linked_list.py
+++ linked_list.py
@@ -34,13 +34,7 @@ class LinkedList:
         return None
 
     def find_link(self, value):
-        link = self._head_link
-        while link is not None:
-            if link.get_value() == value:
-                return True
-            else:
-                link = link.get_next()
-        return False
+        return self._find_link(value) is not None
 
     def add_link_at_start(self, value):
         new_link = _Link(value)
@@ -62,22 +56,26 @@ class LinkedList:
         if link_to_find == self._head_link:
             self._head_link = link_to_find.get_next()
             link_to_find.set_next(None)
+            return link_to_find.value
         elif link_to_find == self._last_link:
             previous_link = self._head_link
             while previous_link.get_next() is not link_to_find:
                 previous_link = previous_link.get_next()
             previous_link.set_next(None)
-        else:
+            return previous_link.valu
+        elif link_to_find != self._head_link and link_to_find != self._last_link and link_to_find is not None:
             previous_link = self._head_link
             while previous_link.get_next() is not link_to_find:
                 previous_link = previous_link.get_next()
             previous_link.set_next(link_to_find.get_next())
             link_to_find.set_next(None)
+            return link_to_find.value
+        else:
+            return None
 
-    def print_collection(self):
-        new_itterator = _Counter(self._head_link)
-        for i in new_itterator:
-            print(i)
+    def __iter__(self):
+        new_iterator = _LinkedListIterator(self._head_link)
+        return new_iterator
 
     def add_link_after(self, link_to_add, value):
         link_to_find = self._find_link(value)
@@ -85,18 +83,17 @@ class LinkedList:
             new_link = _Link(link_to_add)
             self._last_link.set_next(new_link)
             self._last_link = new_link
-        else:
+        elif link_to_find != self._last_link and link_to_find is not None:
             new_link = _Link(link_to_add)
             new_link.set_next(link_to_find.get_next())
             link_to_find.set_next(new_link)
+        else:
+            print(value, "is not in the list so you will better use an element that is in it")
 
 
 class _LinkedListIterator:
     def __init__(self, current):
         self._current = current
-
-    def __iter__(self):
-        return self
 
     def __next__(self):
         if self._current is None:
@@ -115,14 +112,20 @@ def test_Linked_List():
     my_list.add_link_at_end("Anat")
     my_list.add_link_at_start("hillel")
     assert my_list.find_link("Amotz")
-    my_list.remove("Amotz")
+    assert my_list.remove("Amotz") == "Amotz"
     assert not my_list.find_link("Amotz")
     my_list.add_link_after("Asaf", "Anat")
-    new_itterator = _LinkedListIterator(my_list._head_link)
-    assert new_itterator.__next__() == "hillel"
-    assert new_itterator.__next__() == "Yotam"
-    assert new_itterator.__next__() == "Anat"
-    assert new_itterator.__next__() == "Asaf"
+    my_list.add_link_after("anat", "yonatan")
+    assert not my_list.find_link("yonatan") is None
+    assert my_list.remove("sara") is None
+    assert not my_list.find_link("sara")
+    for i in my_list:
+        print(i)
+    my_iterator = my_list.__iter__()
+    assert my_iterator.__next__() == "hillel"
+    assert my_iterator.__next__() == "Yotam"
+    assert my_iterator.__next__() == "Anat"
+    assert my_iterator.__next__() == "Asaf"
 
 
 test_Linked_List()
