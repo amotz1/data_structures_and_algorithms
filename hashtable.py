2ef2a772031ee1a2adb747f4b6999a57c76d3894
amotz1 <31002831+amotz1@users.noreply.github.com>
2019-12-24 17:08:51 +0200

    Add files via upload

--- hashtable.py
+++ hashtable.py
@@ -2,22 +2,18 @@ import linked_list
 
 
 # [amotz]
-# hashtable partial implementation currently int and string are supported
-# (fixed the bug that you mentioned and now have test for it)
-# i added also remove method with tests
-# i also changed the tuple implementation to my linked_list in preparation to work on collisions.
-# i temporarily changed our get method from get(key) to get(key, value)
-# because we might have more then two values in our linked list (open for new solutions)
-
-# collisions and array automatic resizing are not supported yet.
+# hashtable partial implementation int and string are supported with only hashtable_representation and put methods
+# the hashtable support collisions but not automatically growing array
 
 def compute_hash_code(key):
     if type(key) == str:
-        list_of_ascii_code_chars = [ord(c) for c in key]
+        list_of_ascii_code_chars = [ord(c) for c in key]  # computing the unicode of chars and putting them in a list
         hash_code = sum(list_of_ascii_code_chars)
         return hash_code
-    if type(key) == int:
+    elif type(key) == int:
         return key
+    else:
+        assert False
 
 
 def test_compute_hash_code():
@@ -30,88 +26,118 @@ def test_compute_hash_code():
 test_compute_hash_code()
 
 
+class HashtableElement:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.key_value_pair = []
+
+    def get_key_value_pair(self):
+        key_value_pair = [self.key, self.value]
+        return key_value_pair
+
+
 class Hashtable:
 
-    def __init__(self, my_list):
-        self.my_list = my_list
+    def __init__(self, backing_array):
+        self.backing_array = backing_array
 
     def put(self, key, value):
         hash_code = compute_hash_code(key)
-        list_index = hash_code % len(self.my_list)
-        if self.my_list[list_index] is None:
-            my_linked_list = linked_list.LinkedList()
-            my_linked_list.add_link_at_end(key)
-            my_linked_list.add_link_at_end(value)
-            self.my_list[list_index] = my_linked_list
+        backing_array_index = hash_code % len(self.backing_array)
+        if self.backing_array[backing_array_index] is None:
+            hashtable_element = HashtableElement(key, value)
+            self.backing_array[backing_array_index] = hashtable_element
         else:
-            self.my_list[list_index].add_link_at_end(key)
-            self.my_list[list_index].add_link_at_end(value)
+            hashtable_element = HashtableElement(key, value)
+            b_a_linked_list = linked_list.LinkedList()
+            b_a_linked_list.add_link_at_end(self.backing_array[backing_array_index])
+            b_a_linked_list.add_link_at_end(hashtable_element)
+            self.backing_array[backing_array_index] = b_a_linked_list
 
-    def get(self, key):
-        hash_code = compute_hash_code(key)
-        list_index = hash_code % len(self.my_list)
-        if self.my_list[list_index] is not None:
-            new_iterator = self.my_list[list_index].__iter__()
-            link_value = new_iterator.__next__()
-            if link_value == key:
-                return self.my_list[list_index].get_last_link()
+    # [amotz]
+    # in my hashtable_representation below
+    # i chose to use a representation of index without brackets and key value pairs with brackets
+
+    def hashtable_representation(self):
+        representation = []
+        for backing_array_index in range(len(self.backing_array)):
+            if type(self.backing_array[backing_array_index]) == linked_list.LinkedList:
+                representation.append(backing_array_index)
+                for b_a_linked_list_index in self.backing_array[backing_array_index]:
+                    representation.append(b_a_linked_list_index.get_key_value_pair())
+            elif type(self.backing_array[backing_array_index]) == HashtableElement:
+                representation.append(backing_array_index)
+                representation.append(self.backing_array[backing_array_index].get_key_value_pair())
             else:
-                link = self.my_list[list_index].find_link(key)
-                next_link = link.get_next()
-                return next_link.get_value()
-
-    def traverse(self):
-        keys_and_values = []
-        for list_index in range(len(self.my_list)):
-            if self.my_list[list_index] is not None:
-                keys_and_values.append(list_index)
-                for linked_list_index in self.my_list[list_index]:
-                    keys_and_values.append([linked_list_index])
-        return keys_and_values
-
-    def remove(self, key):
-        hash_code = compute_hash_code(key)
-        list_index = hash_code % len(self.my_list)
-        if self.my_list[list_index].size() == 2:
-            self.my_list[list_index] = None
-        else:
-            link = self.my_list[list_index].find_link(key)
-            next_link = link.get_next()
-            self.my_list[list_index].remove(next_link.get_value)
-            self.my_list[list_index].remove(link.get_value)
-
+                representation.append(backing_array_index)
+        return representation
 
 def test_Hashtable():
     amotz_hashtable = Hashtable([None] * 10)
-    assert amotz_hashtable.get(6) is None
-    assert amotz_hashtable.get(8) is None
-    assert amotz_hashtable.get(9) is None
+    # putting a value in a key that is int that is not in the hashtable
     amotz_hashtable.put(1000006, 1001)
+    # putting a value in a key that is int that is already in the hashtable
     amotz_hashtable.put(1006, 142)
-    amotz_hashtable.put(299, 765)
-    amotz_hashtable.put(9, 32)
-    assert amotz_hashtable.traverse() == [6, [1000006], [1001], [1006], [142], 9, [299], [765], [9], [32]]
-    assert amotz_hashtable.get(9) == 32
-    assert amotz_hashtable.get(8) is None
-    amotz_hashtable.put(8, 123)
-    assert amotz_hashtable.get(8) == 123
-    amotz_hashtable.put("amotz", 30)
-    assert amotz_hashtable.get("amotz") == 30
-    assert amotz_hashtable.get("yotam") is None
-    amotz_hashtable.put("hillel", 38)
-    assert amotz_hashtable.get("hillel") == 38
-    amotz_hashtable.put("anat", 70)
-    assert amotz_hashtable.get("anat") == 70
-    print(amotz_hashtable.traverse())
-    amotz_hashtable.remove('anat')
-    assert amotz_hashtable.get("anat") is None
-    amotz_hashtable.remove(8)
-    assert amotz_hashtable.get(8) is None
-    print(amotz_hashtable.traverse())
-    amotz_hashtable.remove(9)
-    print(amotz_hashtable.traverse())
-    assert amotz_hashtable.get(299) == 765
-
+    # putting values in a key that is a string
+    amotz_hashtable.put('amotz', 30)
+    # testing that every element is put in the right places of the hashtable
+    assert amotz_hashtable.hashtable_representation() == [0, 1, 2, 3, 4, 5, ['amotz', 30],
+                                                          6, [1000006, 1001], [1006, 142],
+                                                          7, 8, 9]
 
 
 test_Hashtable()
+
+# from here on its the rest of the code that i still need to clean and rewrite
+# ------------------------------------------------------------------------------------------------
+# assert amotz_hashtable.get(6) is None
+# assert amotz_hashtable.get(8) is None
+# assert amotz_hashtable.get(9) is None
+# assert amotz_hashtable.get(9) == 32
+# assert amotz_hashtable.get(8) is None
+# amotz_hashtable.put(8, 123)
+# assert amotz_hashtable.get(8) == 123
+# amotz_hashtable.put("amotz", 30)
+# assert amotz_hashtable.get("amotz") == 30
+# assert amotz_hashtable.get("yotam") is None
+# amotz_hashtable.put("hillel", 38)
+# assert amotz_hashtable.get("hillel") == 38
+# amotz_hashtable.put("anat", 70)
+# assert amotz_hashtable.get("anat") == 70
+# print(amotz_hashtable.Hashtable_representation())
+# amotz_hashtable.remove('anat')
+# assert amotz_hashtable.get("anat") is None
+# amotz_hashtable.remove(8)
+# assert amotz_hashtable.get(8) is None
+# print(amotz_hashtable.Hashtable_representation())
+# amotz_hashtable.remove(9)
+# print(amotz_hashtable.Hashtable_representation())
+# assert amotz_hashtable.get(299) == 765
+
+
+# from here it is the rest of the code that i still need to clean
+# --------------------------------------------------------------------------------------------------
+#     def get(self, key):
+#         hash_code = compute_hash_code(key)
+#         backing_array_index = hash_code % len(self.backing_array)
+#         if self.backing_array[backing_array_index] is not None:
+#             new_iterator = self.backing_array[backing_array_index].__iter__()
+#             link_value = new_iterator.__next__()
+#             if link_value == key:
+#                 return self.backing_array[backing_array_index].get_last_link()
+#             else:
+#                 link = self.backing_array[backing_array_index].find_link(key)
+#                 next_link = link.get_next()
+#                 return next_link.get_value()
+#
+#     def remove(self, key):
+#         hash_code = compute_hash_code(key)
+#         backing_array_index = hash_code % len(self.backing_array)
+#         if self.backing_array[backing_array_index].size() == 2:
+#             self.backing_array[backing_array_index] = None
+#         else:
+#             link = self.backing_array[backing_array_index].find_link(key)
+#             next_link = link.get_next()
+#             self.backing_array[backing_array_index].remove(next_link.get_value)
+#             self.backing_array[backing_array_index].remove(link.get_value)
