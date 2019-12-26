05745ad3f07eccd63c17dc6ed8a2e3073eb6438b
amotz1 <31002831+amotz1@users.noreply.github.com>
2019-12-26 18:26:28 +0200

    Add files via upload

--- hashtable.py
+++ hashtable.py
@@ -13,7 +13,7 @@ def compute_hash_code(key):
     elif type(key) == int:
         return key
     else:
-        assert False
+        assert False, "only int and string are supported"
 
 
 def test_compute_hash_code():
@@ -26,34 +26,32 @@ def test_compute_hash_code():
 test_compute_hash_code()
 
 
-class HashtableElement:
+class KeyValuePair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.key_value_pair = []
-
-    def get_key_value_pair(self):
-        key_value_pair = [self.key, self.value]
-        return key_value_pair
 
 
 class Hashtable:
 
-    def __init__(self, backing_array):
-        self.backing_array = backing_array
+    def __init__(self):
+        self.backing_array = [None]*10
 
     def put(self, key, value):
         hash_code = compute_hash_code(key)
-        backing_array_index = hash_code % len(self.backing_array)
-        if self.backing_array[backing_array_index] is None:
-            hashtable_element = HashtableElement(key, value)
-            self.backing_array[backing_array_index] = hashtable_element
-        else:
-            hashtable_element = HashtableElement(key, value)
+        index = hash_code % len(self.backing_array)
+        if self.backing_array[index] is None:
+            hashtable_element = KeyValuePair(key, value)
+            self.backing_array[index] = hashtable_element
+        elif type(self.backing_array[index]) == KeyValuePair:
+            hashtable_element = KeyValuePair(key, value)
             b_a_linked_list = linked_list.LinkedList()
-            b_a_linked_list.add_link_at_end(self.backing_array[backing_array_index])
+            b_a_linked_list.add_link_at_end(self.backing_array[index])
             b_a_linked_list.add_link_at_end(hashtable_element)
-            self.backing_array[backing_array_index] = b_a_linked_list
+            self.backing_array[index] = b_a_linked_list
+        else:
+            hashtable_element = KeyValuePair(key, value)
+            self.backing_array[index].add_link_at_end(hashtable_element)
 
     # [amotz]
     # in my hashtable_representation below
@@ -61,29 +59,32 @@ class Hashtable:
 
     def hashtable_representation(self):
         representation = []
-        for backing_array_index in range(len(self.backing_array)):
-            if type(self.backing_array[backing_array_index]) == linked_list.LinkedList:
-                representation.append(backing_array_index)
-                for b_a_linked_list_index in self.backing_array[backing_array_index]:
-                    representation.append(b_a_linked_list_index.get_key_value_pair())
-            elif type(self.backing_array[backing_array_index]) == HashtableElement:
-                representation.append(backing_array_index)
-                representation.append(self.backing_array[backing_array_index].get_key_value_pair())
+        for index in range(len(self.backing_array)):
+            if type(self.backing_array[index]) == linked_list.LinkedList:
+                representation.append(index)
+                for linked_list_index in self.backing_array[index]:
+                    representation.append([linked_list_index.key, linked_list_index.value])
+            elif type(self.backing_array[index]) == KeyValuePair:
+                representation.append(index)
+                representation.append([self.backing_array[index].key, self.backing_array[index].value])
             else:
-                representation.append(backing_array_index)
+                representation.append(index)
         return representation
 
+
 def test_Hashtable():
-    amotz_hashtable = Hashtable([None] * 10)
-    # putting a value in a key that is int that is not in the hashtable
+    amotz_hashtable = Hashtable()
+    # putting a value in a key that is int that is not in the hashtable-testing support for int
     amotz_hashtable.put(1000006, 1001)
-    # putting a value in a key that is int that is already in the hashtable
+    # putting a value in a key that is int that is already in the hashtable- testing first collision
     amotz_hashtable.put(1006, 142)
-    # putting values in a key that is a string
+    # putting another value that is int in the index 6 to have 3 elements in the 6 index - testing second collision
+    amotz_hashtable.put(10000006, 35)
+    # putting a value in a key that is a string-testing support for strings
     amotz_hashtable.put('amotz', 30)
     # testing that every element is put in the right places of the hashtable
     assert amotz_hashtable.hashtable_representation() == [0, 1, 2, 3, 4, 5, ['amotz', 30],
-                                                          6, [1000006, 1001], [1006, 142],
+                                                          6, [1000006, 1001], [1006, 142], [10000006, 35],
                                                           7, 8, 9]
 
 
@@ -120,24 +121,24 @@ test_Hashtable()
 # --------------------------------------------------------------------------------------------------
 #     def get(self, key):
 #         hash_code = compute_hash_code(key)
-#         backing_array_index = hash_code % len(self.backing_array)
-#         if self.backing_array[backing_array_index] is not None:
-#             new_iterator = self.backing_array[backing_array_index].__iter__()
+#         index = hash_code % len(self.backing_array)
+#         if self.backing_array[index] is not None:
+#             new_iterator = self.backing_array[index].__iter__()
 #             link_value = new_iterator.__next__()
 #             if link_value == key:
-#                 return self.backing_array[backing_array_index].get_last_link()
+#                 return self.backing_array[index].get_last_link()
 #             else:
-#                 link = self.backing_array[backing_array_index].find_link(key)
+#                 link = self.backing_array[index].find_link(key)
 #                 next_link = link.get_next()
 #                 return next_link.get_value()
 #
 #     def remove(self, key):
 #         hash_code = compute_hash_code(key)
-#         backing_array_index = hash_code % len(self.backing_array)
-#         if self.backing_array[backing_array_index].size() == 2:
-#             self.backing_array[backing_array_index] = None
+#         index = hash_code % len(self.backing_array)
+#         if self.backing_array[index].size() == 2:
+#             self.backing_array[index] = None
 #         else:
-#             link = self.backing_array[backing_array_index].find_link(key)
+#             link = self.backing_array[index].find_link(key)
 #             next_link = link.get_next()
-#             self.backing_array[backing_array_index].remove(next_link.get_value)
-#             self.backing_array[backing_array_index].remove(link.get_value)
+#             self.backing_array[index].remove(next_link.get_value)
+#             self.backing_array[index].remove(link.get_value)
