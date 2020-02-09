2d9d514db7c8c4785bf069ebfb287468d7e97395
amotz taub tabib <amotz.tt@gmail.com>
2020-02-09 10:20:32 +0200

    adding remove method and changing some tests and stuff

--- hashtable.py
+++ hashtable.py
@@ -57,9 +57,7 @@ class Hashtable:
                 if i.key == key:
                     is_key_found = True
                     i.value = value
-                else:
-                    pass
-            if is_key_found is False:
+            if not is_key_found:
                 hashtable_element = KeyValuePair(key, value)
                 self.backing_array[index].add_link_at_end(hashtable_element)
 
@@ -80,10 +78,22 @@ class Hashtable:
                     return i.value
             return None
 
-    # [amotz]
-    # in my hashtable_representation below
-    # i chose to use a representation of hashtable in which
-    # the indexes  of the backing array are of type int and the key value pairs are of type python list
+            # [amotz]
+            # in my hashtable_representation below
+            # i chose to use a representation of hashtable in which
+            # the indexes  of the backing array are of type int and the key value pairs are of type python list
+
+    def remove(self, key):
+        hash_code = compute_hash_code(key)
+        index = hash_code % len(self.backing_array)
+        if type(self.backing_array[index]) == KeyValuePair:
+            self.backing_array[index] = None
+        elif type(self.backing_array[index]) == linked_list.LinkedList:
+            for i in self.backing_array[index]:
+                if i.key == key:
+                    self.backing_array[index].remove(i)
+        else:
+            print("the number  is not in the hashtable so cannot be removed")
 
     def hashtable_representation(self):
         representation = []
@@ -110,7 +120,11 @@ def test_Hashtable():
     # testing basic put and get
     hashtable.put(1, 1)
     assert hashtable.get(1) == 1
-    # testing a different key that computes to index 1 also but is not in the hashtable
+    # testing a different key that will be stored in the same bucket but is not in the hashtable
+    # (the bucket contains a KeyValuePair object)
+    assert (compute_hash_code(11) % len(hashtable.backing_array)
+            == compute_hash_code(1) % len(hashtable.backing_array))
+    assert type(hashtable.backing_array[1]) == KeyValuePair
     assert hashtable.get(11) is None
 
     # testing hash collision
@@ -120,29 +134,56 @@ def test_Hashtable():
     hashtable.put('cab', 3)
     assert hashtable.get('abc') == 2
     assert hashtable.get('cab') == 3
-    # testing a key that computes to the same index as the keys above but is not in the hashtable
+    # testing a key that will be stored in the same bucket as the keys above but is not in the hashtable
+    # (this time the bucket contains a linked list)
+    assert type(hashtable.backing_array[4]) == linked_list.LinkedList
     assert hashtable.get('bac') is None
-    # the key 'abc' and 'cab' computes to the index 4,
-    # i add another key that computes to the index 4 to test the put method
-    # when the backing array index has a linked list - test for second collision
+    # showing that the index of the bucket that stores 'abc' and 'bac is 4 for later testing
+    assert (compute_hash_code("abc") % len(hashtable.backing_array)
+            == compute_hash_code("acb") % len(hashtable.backing_array) == 4)
+    # i add another key that will be stored in the same bucket as 'abc and 'bac' to test the put method
+    # when the backing array bucket has a linked list - test for second collision
     hashtable.put(4, 1)
     assert hashtable.get(4) == 1
-    # testing that putting the third key value pair in index 4 didn't change the other key value pairs in index 4
+    # testing that putting the third key value pair in the same bucket didn't
+    # change the other key value pairs in the bucket of index 4
     assert hashtable.get('abc') == 2
     assert hashtable.get('cab') == 3
 
-    # putting a value in a key that is already in the hashtable and is in
-    # a KeyValuePair object that is not in a linked list
+    # putting a value in a key that is already in the hashtable in a bucket that contains
+    # a KeyValuePair object
     # testing update to a key
+    assert hashtable.get(1) is not None
+    assert type(hashtable.backing_array[1]) == KeyValuePair
     hashtable.put(1, 2)
     assert hashtable.get(1) == 2
     # putting a value in a key that is already in the hashtable and is in a linked list of KeyValuePair objects
     # testing update to a key
+    assert hashtable.get(4) is not None
+    assert type(hashtable.backing_array[4]) == linked_list.LinkedList
     hashtable.put(4, 3)
     assert hashtable.get(4) == 3
 
     # testing hashtable_representation method
     assert hashtable.hashtable_representation() == [0, 1, [1, 2], 2, 3, 4, ['abc', 2], ['cab', 3], [4, 3], 5,
+                                                    6, 7, 8, 9]
+
+    # test of removing a key in a KeyValuePair object that is not in a linked list object
+    assert type(hashtable.backing_array[1]) == KeyValuePair
+    hashtable.remove(1)
+    assert hashtable.get(1) is None
+    # test of removing a key that is in a LinkedList object
+    assert type(hashtable.backing_array[4]) == linked_list.LinkedList
+    hashtable.remove('cab')
+    assert hashtable.get('cab') is None
+    # test that all the key value pairs in the hashtable are still in the right places
+    assert hashtable.hashtable_representation() == [0, 1, 2, 3, 4, ['abc', 2], [4, 3], 5,
+                                                    6, 7, 8, 9]
+    hashtable.remove(2)
+    # test of removing a key that is not in the hashtable
+    assert hashtable.get(2) is None
+    # test that all the key value pairs in the hashtable are still in the right places
+    assert hashtable.hashtable_representation() == [0, 1, 2, 3, 4, ['abc', 2], [4, 3], 5,
                                                     6, 7, 8, 9]
 
 
@@ -179,13 +220,3 @@ test_Hashtable()
 # --------------------------------------------------------------------------------------------------
 
 #
-#     def remove(self, key):
-#         hash_code = compute_hash_code(key)
-#         index = hash_code % len(self.backing_array)
-#         if self.backing_array[index].size() == 2:
-#             self.backing_array[index] = None
-#         else:
-#             link = self.backing_array[index].find_link(key)
-#             next_link = link.get_next()
-#             self.backing_array[index].remove(next_link.get_value)
-#             self.backing_array[index].remove(link.get_value)
