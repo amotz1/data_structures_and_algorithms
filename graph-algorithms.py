4956808a6238d80025adaffbb88acc284fc9e7be
amotz taub tabib <amotz.tt@gmail.com>
2020-04-03 18:45:56 +0300

    new project on graphs

---
+++ graph-algorithms.py
@@ -0,0 +1,23 @@
+import linked_list
+
+class Graph:
+    def __init__(self):
+        self.list = []
+
+    def add_vertex(self, label):
+        list = linked_list.LinkedList
+        list.add_link_at_end(label)
+        self.list.append(list)
+
+    def add_edge(self, src, dest):
+
+    def add_weight(self, weight):
+
+class Que:
+    def __init__(self):
+
+
+
+class Stack:
+    def __init__(self):
+        
