b6b356dc2a35d69078d2771f8b833722958bd755
amotz taub tabib <amotz.tt@gmail.com>
2020-04-09 20:07:20 +0300

    minor change

--- graph-algorithms.py
+++ graph-algorithms.py
@@ -10,7 +10,7 @@ class Graph:
 
     def traverse(self):
 
-    def efferent_neurons_sum(self):
+    def edges_weights_sum(self):
 
     def get_neighbors(self):
 
@@ -49,12 +49,11 @@ def test_Graph():
     brain_network.add_edge('amigdala', 'frontal_lobe', 8)
     brain_network.add_edge('hypocampus', 'frontal_lobe', 10)
     assert brain_network.traverse() == ['brain_stem','hypocampus','amigdala','frontal_lobe']
-    # efferent nerves are neurons that goes from the vertex out to other vertcies (like edges weight)
-    assert brain_network.efferent_neurons_sum('amigdala') == 8
-    assert brain_network.efferent_neurons_sum('frontal_lobe') == 0
-    assert brain_network.efferent_neurons_sum('hypocampus') == 10
-    assert brain_network.efferent_neurons_sum('thalamus') == 5
-    assert brain_network.efferent_neurons_sum('brain_stem') == 13
+    assert brain_network.edges_weights_sum('amigdala') == 8
+    assert brain_network.edges_weights_sum('frontal_lobe') == 0
+    assert brain_network.edges_weights_sum('hypocampus') == 10
+    assert brain_network.edges_weights_sum('thalamus') == 5
+    assert brain_network.edges_weights_sum('brain_stem') == 13
     assert brain_network.get_neighbors('brain_stem') == ['hypocampus', 'amigdala', 'thalamus']
     assert brain_network.most_connected_path('brain_stem','frontal_lobe') == ['brain_stem' , 'hypocampus', 'frontal_lobe', 15]
     assert brain_network.least_connected_path('brain_stem','frontal_lobe') == ['brain_stem', 'thalamus', 'frontal_lobe', 8]
