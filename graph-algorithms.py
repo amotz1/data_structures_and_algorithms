9b86d7a42f2b1fd3475217d723f4c6b4de3f8acb
amotz taub tabib <amotz.tt@gmail.com>
2020-04-23 18:44:50 +0300

    addressing your comments

--- graph-algorithms.py
+++ graph-algorithms.py
@@ -4,36 +4,33 @@ import hashtable
 import hashtable
 
 
-# partial graph implementation
+# partial implementation of a directed graph
 # the graph supports vertices and edges
 
 class Graph:
     def __init__(self) -> object:
-        self.vertices = []
-        self.hash = hashtable.Hashtable()
+        self.label2vertex_or_edge = hashtable.Hashtable()
 
-    # [amotz] i wasnt sure here on your comment regarding the name add_vertex (and possibly add_edge also)
-    # would you suggest to add another function called create_vertex and use it in this function?
-    def add_vertex(self, label):
+    def create_vertex(self, label):
         vertex = Vertex(label)
-        self.hash.put(label, vertex)
-        self.vertices.append(vertex)
+        self.label2vertex_or_edge.put(label, vertex)
 
-    def add_edge(self, src, dest, weight):
+    def create_edge(self, src, dest, weight):
         edge = Edge(src, dest, weight)
-        vertex = self.hash.get(src)
-        vertex.add_edge(edge)
+        self.label2vertex_or_edge.put(edge.label, edge)
+        src = self.label2vertex_or_edge.get(src.label)
+        dest = self.label2vertex_or_edge.get(dest.label)
+        src.add_edge(edge)
+        dest.add_edge(edge)
 
     def get_vertex(self, label):
-        vertex = self.hash.get(label)
+        vertex = self.label2vertex_or_edge.get(label)
         return vertex
 
     def get_edge(self, src, dest):
-        vertex = self.get_vertex(src)
-        for i in vertex.edges:
-            if i.src == src and i.dest == dest:
-                return i
-        return None
+        edge_label = '{}-{}'.format(src.label, dest.label)
+        edge = self.label2vertex_or_edge.get(edge_label)
+        return edge
 
     # def traverse(self):
     #
@@ -59,53 +56,48 @@ class Vertex:
 
 class Edge:
     def __init__(self, src, dest, weight):
+        self.label = '{}-{}'.format(src.label, dest.label)
         self.src = src
         self.dest = dest
         self.weight = weight
 
 
 def test_Graph():
-    brain_network = Graph()
-    brain_network.add_vertex('hypocampus')
+    brain_network: Graph = Graph()
     assert brain_network.get_vertex('hillel') is None
-    brain_region = brain_network.get_vertex('hypocampus')
-    assert brain_region.label == 'hypocampus'
-    brain_network.add_vertex('thalamus')
-    brain_region = brain_network.get_vertex('thalamus')
-    assert brain_region.label == 'thalamus'
-    brain_network.add_vertex('amygdala')
-    brain_region = brain_network.get_vertex('amygdala')
-    assert brain_region.label == 'amygdala'
-    brain_network.add_vertex('frontal_lobe')
-    brain_region = brain_network.get_vertex('frontal_lobe')
-    assert brain_region.label == 'frontal_lobe'
-    brain_network.add_vertex('brain_stem')
-    brain_region = brain_network.get_vertex('brain_stem')
-    assert brain_region.label == 'brain_stem'
-    brain_network.add_edge('brain_stem', 'thalamus', 3)
-    nerve = brain_network.get_edge('brain_stem', 'thalamus')
-    assert nerve.src == 'brain_stem'
-    assert nerve.dest == 'thalamus'
-    brain_network.add_edge('brain_stem', 'amygdala', 5)
-    nerve = brain_network.get_edge('brain_stem', 'amygdala')
-    assert nerve.src == 'brain_stem'
-    assert nerve.dest == 'amygdala'
-    brain_network.add_edge('brain_stem', 'hypocampus', 5)
-    nerve = brain_network.get_edge('brain_stem', 'hypocampus')
-    assert nerve.src == 'brain_stem'
-    assert nerve.dest == 'hypocampus'
-    brain_network.add_edge('thalamus', 'frontal_lobe', 5)
-    nerve = brain_network.get_edge('thalamus', 'frontal_lobe')
-    assert nerve.src == 'thalamus'
-    assert nerve.dest == 'frontal_lobe'
-    brain_network.add_edge('amygdala', 'frontal_lobe', 8)
-    nerve = brain_network.get_edge('amygdala', 'frontal_lobe')
-    assert nerve.src == 'amygdala'
-    assert nerve.dest == 'frontal_lobe'
-    brain_network.add_edge('hypocampus', 'frontal_lobe', 10)
-    nerve = brain_network.get_edge('hypocampus', 'frontal_lobe')
-    assert nerve.src == 'hypocampus'
-    assert nerve.dest == 'frontal_lobe'
+    for vertex in ['hypocampus', 'thalamus', 'amygdala', 'frontal_lobe', 'brain_stem']:
+        brain_network.create_vertex(vertex)
+        brain_region = brain_network.get_vertex(vertex)
+        assert brain_region.label == vertex
+    brain_stem = brain_network.get_vertex('brain_stem')
+    thalamus = brain_network.get_vertex('thalamus')
+    amygdala = brain_network.get_vertex('amygdala')
+    frontal_lobe = brain_network.get_vertex('frontal_lobe')
+    hypocampus = brain_network.get_vertex('hypocampus')
+    brain_network.create_edge(brain_stem, thalamus, 3)
+    nerve = brain_network.get_edge(brain_stem, thalamus)
+    assert nerve.src == brain_stem
+    assert nerve.dest == thalamus
+    brain_network.create_edge(brain_stem, amygdala, 5)
+    nerve = brain_network.get_edge(brain_stem, amygdala)
+    assert nerve.src == brain_stem
+    assert nerve.dest == amygdala
+    brain_network.create_edge(brain_stem, hypocampus, 5)
+    nerve = brain_network.get_edge(brain_stem, hypocampus)
+    assert nerve.src == brain_stem
+    assert nerve.dest == hypocampus
+    brain_network.create_edge(thalamus, frontal_lobe, 5)
+    nerve = brain_network.get_edge(thalamus, frontal_lobe)
+    assert nerve.src == thalamus
+    assert nerve.dest == frontal_lobe
+    brain_network.create_edge(amygdala, frontal_lobe, 8)
+    nerve = brain_network.get_edge(amygdala, frontal_lobe)
+    assert nerve.src == amygdala
+    assert nerve.dest == frontal_lobe
+    brain_network.create_edge(hypocampus, frontal_lobe, 10)
+    nerve = brain_network.get_edge(hypocampus, frontal_lobe)
+    assert nerve.src == hypocampus
+    assert nerve.dest == frontal_lobe
     # assert brain_network.traverse() == ['brain_stem','hypocampus','amygdala','frontal_lobe']
     # assert brain_network.edges_weights_sum('amygdala') == 8
     # assert brain_network.edges_weights_sum('frontal_lobe') == 0
